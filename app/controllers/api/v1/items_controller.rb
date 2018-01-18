c1a2c58a57637e530cb74b5bb36e616f7036721b
Marcos Martins <marcos.martins2307@gmail.com>
2018-01-18 13:29:33 -0700

    Added namespace to routes PART II

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,43 +1,45 @@
-class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :update, :destroy]
+module Api::V1
+  class ItemsController < ApplicationController
+    before_action :set_item, only: [:show, :update, :destroy]
 
-  # GET /items
-  def index
-    @items = Item.all
-    json_response(@items)
-  end
+    # GET /items
+    def index
+      @items = Item.all
+      json_response(@items)
+    end
 
-  # POST /items
-  def create
-    @item = Item.create!(item_params)
-    json_response(@item, :created)
-  end
+    # POST /items
+    def create
+      @item = Item.create!(item_params)
+      json_response(@item, :created)
+    end
 
-  # GET /items/:id
-  def show
-    json_response(@item)
-  end
+    # GET /items/:id
+    def show
+      json_response(@item)
+    end
 
-  # PUT /items/:id
-  def update
-    @item.update(item_params)
-    head :no_content
-  end
+    # PUT /items/:id
+    def update
+      @item.update(item_params)
+      head :no_content
+    end
 
-  # DELETE /items/:id
-  def destroy
-    @item.destroy
-    head :no_content
-  end
+    # DELETE /items/:id
+    def destroy
+      @item.destroy
+      head :no_content
+    end
 
-  private
+    private
 
-  def item_params
-    # whitelist params
-    params.permit(:title, :created_by)
-  end
+    def item_params
+      # whitelist params
+      params.permit(:title, :created_by)
+    end
 
-  def set_item
-    @item = Item.find(params[:id])
+    def set_item
+      @item = Item.find(params[:id])
+    end
   end
 end
