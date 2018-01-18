4522869a804dedc65c1bc91ec5f7395a5967c720
Marcos Martins <marcos.martins2307@gmail.com>
2018-01-18 13:25:00 -0700

    Added namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :items do
-  	resources :points
+  namespace :api do
+  	namespace :v1 do
+	  resources :items do
+	    resources :points
+	  end
+    end
   end
 end
